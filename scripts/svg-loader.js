35ccb40dd3c689d8f4dbc3582bef33b775e3ed47
JehhB <jwy.bayod@gmail.com>
2023-02-05 10:47:09 +0800

    Added hero section

---
+++ scripts/svg-loader.js
@@ -0,0 +1,27 @@
+/** insert SVG into dom */
+function loadSVG() {
+  const SVG_ELEMENTS = document.querySelectorAll("svg-element");
+  SVG_ELEMENTS.forEach(function (element) {
+    const XHR_REQUEST = new XMLHttpRequest();
+
+    XHR_REQUEST.open("GET", element.dataset.src);
+    XHR_REQUEST.onreadystatechange = function () {
+      if (
+        XHR_REQUEST.readyState !== XMLHttpRequest.DONE ||
+        XHR_REQUEST.status !== 200
+      )
+        return;
+
+      const TEMP_CONTAINER = document.createElement("div");
+      TEMP_CONTAINER.innerHTML = XHR_REQUEST.responseText;
+      const SVG = TEMP_CONTAINER.firstChild;
+
+      const CLASSNAMES = element.className;
+      SVG.setAttribute("class", CLASSNAMES);
+
+      const PARENT_ELEMENT = element.parentNode;
+      PARENT_ELEMENT.replaceChild(SVG, element);
+    };
+    XHR_REQUEST.send();
+  });
+}
